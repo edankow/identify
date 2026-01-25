fe608d69575bb9aa2c6303cfe362410e791ff3e5
edankow <edankowicz@gmail.com>
2026-01-25 13:27:23 -0500

    Add files via upload

--- traits.js
+++ traits.js
@@ -5,7 +5,7 @@ const traits = [
         id: 'Headlength',
         title: 'How long is head? (difficult to assess; character taken from Whitworth 2006)',
         weight: 2,
-        link: 'Lucilia_US_Canada.pdf#page=17',
+        link: 'lucilia_us_canada.pdf#page=17',
         options: [
             { 
                 label: 'Head shorter; length of head at level of lunule less than half head height (Whitworth 2006)', 
@@ -21,7 +21,7 @@ const traits = [
         id: 'Fronsnarrowing',
         title: 'Frons shape (to separate males and females)',
         weight: 9,
-        link: 'Lucilia_US_Canada.pdf#page=5',
+        link: 'lucilia_us_canada.pdf#page=5',
         options: [
             { 
                 label: 'conspicuously narrowed above antennal bases', 
@@ -37,7 +37,7 @@ const traits = [
         id: 'FronswidthatnarrowesttohelpIDmales',
         title: 'Frons more than 7% of head?',
         weight: 5,
-        link: 'Lucilia_US_Canada.pdf#page=9',
+        link: 'lucilia_us_canada.pdf#page=9',
         options: [
             { 
                 label: 'at least 7% of the head width', 
@@ -53,7 +53,7 @@ const traits = [
         id: 'FronswidthatnarrowesttohelpIDfemales',
         title: 'Frons more than 35% of head width?',
         weight: 3,
-        link: 'Lucilia_US_Canada.pdf#page=11',
+        link: 'lucilia_us_canada.pdf#page=11',
         options: [
             { 
                 label: 'less than 35% head width', 
@@ -69,7 +69,7 @@ const traits = [
         id: 'Frontoorbitalplatesformaleswithnarrowfrons',
         title: 'How widely separated are fronto-orbital plates?',
         weight: 5,
-        link: 'Lucilia_US_Canada.pdf#page=12',
+        link: 'lucilia_us_canada.pdf#page=12',
         options: [
             { 
                 label: 'strongly separated. See figures in Jones et al. 2019, p.174, 180', 
@@ -89,7 +89,7 @@ const traits = [
         id: 'Fronswidthatnarrowestformales',
         title: 'Round or estimate frons width at narrowest',
         weight: 3,
-        link: 'Lucilia_US_Canada.pdf#page=9',
+        link: 'lucilia_us_canada.pdf#page=9',
         options: [
             { 
                 label: 'about 20% head width', 
@@ -125,7 +125,7 @@ const traits = [
         id: 'fronswidthvscuprinamale',
         title: 'Frons more than 15% of head width?',
         weight: 5,
-        link: 'Lucilia_US_Canada.pdf#page=9',
+        link: 'lucilia_us_canada.pdf#page=9',
         options: [
             { 
                 label: 'at least about 20% head width', 
@@ -157,7 +157,7 @@ const traits = [
         id: 'IVSorientation',
         title: 'Do inner vertical setae converge?',
         weight: 2,
-        link: 'Lucilia_US_Canada.pdf#page=30',
+        link: 'lucilia_us_canada.pdf#page=30',
         options: [
             { 
                 label: 'nearly parallel in dorsal view', 
@@ -321,7 +321,7 @@ const traits = [
         id: 'Bodycolor',
         title: 'Color of thorax or abdomen? (unreliable for dead flies)',
         weight: 5,
-        link: 'Lucilia_US_Canada.pdf#page=27',
+        link: 'lucilia_us_canada.pdf#page=27',
         options: [
             { 
                 label: 'coppery brown', 
@@ -421,7 +421,7 @@ const traits = [
         id: 'acrostichalspostsutural',
         title: '# of post-sutural acrostichal pairs.',
         weight: 7,
-        link: 'Lucilia_US_Canada.pdf#page=19',
+        link: 'lucilia_us_canada.pdf#page=19',
         options: [
             { 
                 label: 'three', 
@@ -485,7 +485,7 @@ const traits = [
         id: 'Basicostacolor',
         title: 'What color is the basicosta? (sometimes ambiguous, see notes)',
         weight: 4,
-        link: 'Lucilia_US_Canada.pdf#page=21',
+        link: 'lucilia_us_canada.pdf#page=21',
         options: [
             { 
                 label: 'orange, yellow, or whitish', 
